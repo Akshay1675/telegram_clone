3e685f6080f769acf8a55109acb04fa1c571ade3
Akshay Desai <112275432+Akshay1675@users.noreply.github.com>
2024-07-17 11:18:45 +0530

    Update Menu.jsx

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,15 +1,58 @@
-import { menuList } from "../utils/constants"
+import { useState } from "react";
+import { menuList } from "../utils/constants";
+import {
+  Archive,
+  Bookmark,
+  CircleFadingPlus,
+  Contact,
+  MoonStar,
+  Settings,
+  Sun,
+} from "lucide-react";
+import { useDispatch, useSelector } from "react-redux";
+import { toggleDarkMode } from "../redux/appSlice";
 
 const Menu = () => {
-  return (
-    <div className="px-2 py-3 mt-14 ml-4 rounded-xl absolute bg-white w-[17rem] shadow-xl">
-        <ul>
-          {menuList.map((m,i) => <li className="px-4 py-1 font-bold hover:bg-gray-100" key={i}>
-            {m}
-          </li>)}
-        </ul>
-    </div>
-  )
-}
+  const dispatch = useDispatch();
+  const darkMode = useSelector((appStore) => appStore.app.darkMode);
 
-export default Menu
+  const handleDarkModeClick = () => {
+    dispatch(toggleDarkMode());
+  };
+
+  console.log(darkMode);
+
+  const icons = [
+    !darkMode ? <MoonStar key={1} /> : <Sun />,
+    <Archive key={2} />,
+    <CircleFadingPlus key={3} />,
+    <Contact key={4} />,
+    <Settings key={5} />,
+    <Bookmark key={1} />,
+  ];
+
+  return (
+    <div className="px-2 py-3 mt-14 ml-4 rounded-xl absolute dark:bg-[#212121] dark:text-white  bg-white w-[17rem] shadow-xl">
+      <ul className="">
+        {menuList.map((m, i) => (
+          <li
+            className="px-2 rounded-lg py-1 cursor-pointer flex items-center font-bold hover:bg-gray-100 dark:hover:bg-gray-600"
+            onClick={
+              m.toLowerCase() === "dark mode" ? handleDarkModeClick : undefined
+            }
+            key={i}
+          >
+            {<span className="ml-2 mr-6 my-1">{icons[i]}</span>}
+            {m.toLowerCase() === "dark mode"
+              ? darkMode
+                ? "Light Mode"
+                : m
+              : m}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Menu;
