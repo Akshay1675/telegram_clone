86838cc2aa9d3118242afab5afad35b4747d290d
Akshay Desai <112275432+Akshay1675@users.noreply.github.com>
2024-07-17 11:20:04 +0530

    Update MessageContainer.jsx

--- src/components/MessageContainer.jsx
+++ src/components/MessageContainer.jsx
@@ -1,19 +1,22 @@
-import { useGetMessages } from "../hooks/useGetMessages"
-import MessageCard from "./MessageCard"
-import MessageHeader from "./MessageHeader"
+import { useGetMessages } from "../hooks/useGetMessages";
+import MessageCard from "./MessageCard";
+import MessageHeader from "./MessageHeader";
 
-const MessageContainer = ({id}) => {
-  
-  const messageList = useGetMessages(id)
-  const sendersData = messageList.filter(message => message.sender.id !== 1)
-  const name = sendersData[0].sender.name
-    
+const MessageContainer = ({ id }) => {
+  const messageList = useGetMessages(id);
+  const sendersData = messageList.filter((message) => message.sender.id !== 1);
+  const name = sendersData[0]?.sender?.name;
+
   return (
-    <div className="md:col-span-8 overflow-y-scroll no-scrollbar h-screen w-full bg-slate-100">
-        <MessageHeader name={name}/>
-        {messageList?.map(message => <MessageCard key={message.id} data={message}/>) }
+    <div className="md:col-span-8 h-screen flex flex-col w-full">
+      <MessageHeader name={name} />
+      <div className="overflow-y-scroll no-scrollbar flex-1">
+        {messageList?.map((message) => (
+          <MessageCard key={message.id} data={message} />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default MessageContainer
+export default MessageContainer;
