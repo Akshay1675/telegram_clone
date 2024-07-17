3bf064170e41edc1620e9ea1c2f33d1ddbd10e96
Akshay Desai <112275432+Akshay1675@users.noreply.github.com>
2024-07-17 11:18:10 +0530

    Update MainContainer.jsx

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,19 +1,17 @@
-import { useState } from "react"
-import ChatsContainer from "./ChatsContainer"
-import Header from "./Header"
-import Menu from "./Menu"
+import { useState } from "react";
+import ChatsContainer from "./ChatsContainer";
+import Header from "./Header";
+import Menu from "./Menu";
 
 const MainContainer = () => {
-    const [menuStatus, setMenuStatus] = useState(false)
-    return (
-        <div className="flex col-span-4">
-        <div className="w-[420px] h-screen ">
-            {menuStatus && <Menu />}
-            <Header menuStatus={menuStatus} setMenuStatus={setMenuStatus}/>
-            <ChatsContainer />
-        </div>
-        </div>
-    )
-}
+  const [menuStatus, setMenuStatus] = useState(false);
+  return (
+    <div className="md:w-[480px] flex flex-col col-span-4 dark:bg-[#212121]  w-screen h-screen absolute">
+      {menuStatus && <Menu />}
+      <Header menuStatus={menuStatus} setMenuStatus={setMenuStatus} />
+      <ChatsContainer />
+    </div>
+  );
+};
 
-export default MainContainer
+export default MainContainer;
