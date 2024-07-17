7a4afba143bc6a69b8463d46e7d041ca5d55a0a3
Akshay Desai <112275432+Akshay1675@users.noreply.github.com>
2024-07-17 11:16:04 +0530

    Update ChatCard.jsx

--- src/components/ChatCard.jsx
+++ src/components/ChatCard.jsx
@@ -1,12 +1,19 @@
-
-const ChatCard = ({data, isActive, onClick}) => {
-
-    
+const ChatCard = ({ data, isActive, onClick }) => {
   return (
-    <div className={`mx-4 my-4 border px-4 py-2 hover:bg-gray-200  rounded-xl ${isActive ? "bg-blue-300" : ""}`} onClick={onClick}>
-        <h2 className="font-semibold text-lg mx-4 my-2">{data.creator.name? data.creator.name : "Akshay Desai"}</h2>
+    <div
+      className={`mx-4 my-4 border  px-4 py-2 ${
+        isActive
+          ? "bg-blue-500 dark:bg-[#8774e1]  text-white hover:bg-blue-500 dark:hover:bg-[#8774e1]"
+          : "hover:bg-gray-200  dark:hover:bg-gray-600"
+      } rounded-xl 
+      }`}
+      onClick={onClick}
+    >
+      <h2 className="font-semibold dark:text-white  text-lg mx-4 my-2">
+        {data.creator.name ? data.creator.name : "Akshay Desai"}
+      </h2>
     </div>
-  )
-}
+  );
+};
 
-export default ChatCard
+export default ChatCard;
