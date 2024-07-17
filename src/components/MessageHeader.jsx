f9cc7ad0d799b9382b7b206d8522ec96d61b106f
Akshay Desai <112275432+Akshay1675@users.noreply.github.com>
2024-07-17 11:20:38 +0530

    Update MessageHeader.jsx

--- src/components/MessageHeader.jsx
+++ src/components/MessageHeader.jsx
@@ -1,18 +1,19 @@
-import { EllipsisVertical, Home, Search } from "lucide-react"
-import { Link } from "react-router-dom"
+import { EllipsisVertical, Home, Search } from "lucide-react";
+import { Link } from "react-router-dom";
 
-const MessageHeader = ({name}) => {
+const MessageHeader = ({ name }) => {
   return (
-    <div className="px-6 py-2 flex justify-between items-center sticky top-0 bg-white">
-        <Link to={"/"}><Home className="md:hidden cursor-pointer"/></Link>
-        <h2 className="font-bold text-lg ">{name ? name : "Akshay Desai"}</h2>
-        <div className="flex items-center ">
-          <Search className="mx-6"/>
-          <EllipsisVertical />
-        </div>
-        
+    <div className="px-6 py-4 shadow-lg flex justify-between items-center sticky top-0 bg-white dark:bg-[#212121] dark:text-white">
+      <Link to={"/"}>
+        <Home className="md:hidden cursor-pointer" />
+      </Link>
+      <h2 className="font-bold text-lg ">{name ? name : "Akshay Desai"}</h2>
+      <div className="flex items-center ">
+        <Search className="mx-6" />
+        <EllipsisVertical />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default MessageHeader
+export default MessageHeader;
