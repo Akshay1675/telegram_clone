05262490fa3e0035a24c4ae6e17c4b7f5fe60c5a
Akshay Desai <112275432+Akshay1675@users.noreply.github.com>
2024-07-17 11:23:02 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,41 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
-import MainContainer from "./components/MainContainer"
-import Messages from "./components/Messages"
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MainContainer from "./components/MainContainer";
+import Messages from "./components/Messages";
+import bgImage from "./assets/download.png";
+import bgImageDark from "./assets/download (1).png";
+import { useSelector } from "react-redux";
 
 function App() {
-
   const appRouter = createBrowserRouter([
     {
-    path: "/",
-    element: <MainContainer />,
+      path: "/",
+      element: <MainContainer />,
     },
     {
       path: "/message",
-      element: <Messages />
+      element: <Messages />,
     },
-])
+  ]);
+
+  const darkMode = useSelector((store) => store.app.darkMode);
 
   return (
-    <div className="">
-     <RouterProvider router={appRouter}/>
+    <div className={`${darkMode ? "dark" : ""}`}>
+      {darkMode ? (
+        <img
+          src={bgImageDark}
+          className=" absolute object-covery md:ml-[495px] md:w-[66vw]  h-screen"
+        />
+      ) : (
+        <img
+          src={bgImage}
+          className="opacity-10 absolute object-covery md:ml-[495px] md:w-[66vw]  h-screen"
+        />
+      )}
+
+      <RouterProvider router={appRouter} />
     </div>
-   
-  )
+  );
 }
 
-export default App
+export default App;
